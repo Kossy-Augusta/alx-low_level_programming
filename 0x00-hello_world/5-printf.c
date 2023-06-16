fd4acf11d22dd615c81ede8993c183178c49e983
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-16 06:02:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - print a value using printf
+ *
+ *Return: ALways o (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
