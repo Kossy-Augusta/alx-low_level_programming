ac6c953c51d3e6c7a59840c881f6f4a3d9ab2448
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-16 21:44:33 -0700

    edited

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints alphabets in reverse
+ *main - Program that prints alphabets in reverse
  *
- * Return: 0 (Success)
+ *Return: 0 (Success)
  */
 
 int main(void)
