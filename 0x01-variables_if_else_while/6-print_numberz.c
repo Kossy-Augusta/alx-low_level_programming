d0902de40f2b5b1ad4b2e166f3b87bafbb0696a1
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-16 19:52:11 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints base 10 numbers
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+        int num = 0;
+
+        for(; num < 10; num++)
+        {
+                putchar(num + '0');
+        }
+
+        putchar('\n');
+
+        return (0);
+}
