18b13bfb568e377c7b1c5b0632db151a7b3625d2
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-21 09:05:38 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * Vtimes_table - prints 9 times table
+ */
+
+void times_table(void)
+{
+	int a, b, result;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			result = a * b;
+			if (b  == 0)
+			{
+				_putchar(result + '0');
+			}
+			if (result < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
