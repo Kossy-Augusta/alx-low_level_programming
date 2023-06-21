9e12d0cebfb320ba0bbb9a3c8a6bffa1be431e50
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-21 02:52:15 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ * @int - integar value
+ *
+ * Return: Always 0
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a *= -1;
+	}
+	else if (a == 0)
+	{
+		a = 0;
+	}
+	else if (a < 0)
+	{
+		a *= 1;
+	}
+	return (a);
+}
