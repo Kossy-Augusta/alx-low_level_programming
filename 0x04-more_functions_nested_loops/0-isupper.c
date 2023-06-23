3a022e16b988242de7631449fde363ac6fb9a6ac
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-23 02:06:17 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h" 
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: Value of character entered
+ *
+ * Return: 1 if c is uppercase, 0 if c is otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+
+}
