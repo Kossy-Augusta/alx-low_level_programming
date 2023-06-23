6fa6d0e3182a00dfa0007b4c543ce47b86bca08a
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-23 07:30:28 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		_putchar(a + '0');
+		a++;
+	
+	}
+	_putchar('\n');
+}
