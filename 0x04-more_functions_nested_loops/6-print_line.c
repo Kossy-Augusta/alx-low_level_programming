7c00ab5a180693a5da611ba32da8c633f3824dea
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-23 09:22:57 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line -  Draws a straight line 
+ * @n: Number of underscores to ne printed
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 1; a <= n; a++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar(95);
+	}
+
+	_putchar('\n');
+}
