72a47dbf11a0cf6bd829c5c18ea5e2e761915fe8
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-27 10:49:33 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,12 @@
 /**
  * swap_int - Swaps the value of two integars
  * @a: Firt value
- * @b Second value
+ * @b: Second value
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int value = *a;
+	*a = *b;
+	*b = value;
 }
