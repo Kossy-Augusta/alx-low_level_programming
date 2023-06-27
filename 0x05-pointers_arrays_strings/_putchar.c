59aaacd9fd180591b7bc90e22a75fc2d67da3d88
Kosy-Augusta <kossyudogu@gmail.com>
2023-06-27 10:22:14 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the stdout
+ * @value: Character to be written to the stdout
+ *
+ * Return: 1 on Success, -1 on error
+ */
+
+int _putchar(char value)
+{
+	return (write(1, &value, 1));
+}
