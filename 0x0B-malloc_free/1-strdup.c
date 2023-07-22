828f3fa015b1aae75375d40e65ccbfc59987ab95
Kosy-Augusta <kossyudogu@gmail.com>
2023-07-22 05:38:51 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,23 +8,23 @@
  */
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+	char *string;
+	int i, r;
 
 	if (str == NULL)
 		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-
-	aaa = malloc(sizeof(char) * (i + 1));
-
-	if (aaa == NULL)
+	string = (char *)malloc(sizeof(char) * i);
+	if (string == NULL)
 		return (NULL);
+	for (r = 0; r < i; r++)
+	{
+		string[r] = str[r];
+	}
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
+	return (string);
 
-	return (aaa);
 }
 
