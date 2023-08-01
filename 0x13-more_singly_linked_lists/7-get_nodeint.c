a65a1cb3b6ed220605d87859e64c4148dba68e6f
Kossy-Augusta <kossyudogu@gmail.com>
2023-08-01 04:05:33 -0700

    updated 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *ptr;
 	unsigned int count = 0;
 
-	if (head == NULL)
-		return (NULL);
 	ptr = head;
 	while (count < index)
 	{
