c3514e763889e96e1ad8ca0b83c47bbf3f7033f9
Kossy-Augusta <kossyudogu@gmail.com>
2023-08-14 00:16:57 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: a NULL terminated string
+ *
+ * Return: 1 on success an -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, content;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR);
+	if (fd == -1)
+		return (-1);
+	content = write(fd, text_content, len);
+	if (content == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
