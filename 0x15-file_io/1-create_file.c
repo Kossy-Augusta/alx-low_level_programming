1e1f442f34d2ae2cc5789e35a78a6413f533f709
Kossy-Augusta <kossyudogu@gmail.com>
2023-08-14 00:31:17 -0700

    updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,10 +21,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
 	content = write(fd, text_content, len);
-	if (content == -1)
+	if (content == -1 || fd  == -1)
 		return (-1);
 	close(fd);
 
