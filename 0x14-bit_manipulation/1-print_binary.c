b59daffa36212ab42f4a3590c8621d33fc2796c8
Kossy-Augsta <kossyudogu@gmail.com>
2023-09-29 02:58:15 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * bitCount - counts the number of bits in a number
+ *@num: number whose bits are to be counted
+ *
+ * Return: number of bits counted
+ */
+int bitCount(unsigned long int num)
+{
+	/**variable to hold the num of bits counted*/
+	int count;
+
+	for (count = 0; num != 0; num >>= 1)
+	{
+		if (num | 1)
+			count++;
+	}
+	if (count == 2)
+		return (0);
+	return (count - 1);
+}
+
+/**
+ * print_binary - prints binary representation of a n
+mber
+ *@n: decimal number to be converted to binary
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int count, i;
+	unsigned long int bitMask;
+
+	count = bitCount(n);
+	/** define bitmask and left shift by number of counts*/
+	bitMask = 1 << count;
+	if (n == 0)
+		_putchar('0');
+	for (i = 0; i <= count; i++)
+	{
+		_putchar(n & bitMask ? '1' : '0');
+		/**left shift value by 1*/
+		n <<= 1;
+	}
+}
