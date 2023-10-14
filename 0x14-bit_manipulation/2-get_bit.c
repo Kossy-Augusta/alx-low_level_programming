38599bb14d4f2d788ca2ea34b30efdfd77bb862b
Kossy-Augsta <kossyudogu@gmail.com>
2023-10-14 12:48:27 -0700

    updated 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,43 +1,21 @@
 #include "main.h"
 
 /**
- * bitCount - conts the num, of bits in a number
- * @num: number
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
  *
- * Return: number of counts
- */
-unsigned int bitCount(unsigned long int num)
-{
-	unsigned int count;
-
-	for (count = 0; num != 0; num <<= 1)
-	{
-		if (num & 1)
-			count++;
-	}
-
-	return (count - 1);
-}
-
-/**
- * get_bit - returns the value of a bit at a given index
- * @n: the number whoser bit is to be returned
- * @index: is the index, starting from 0 of the bit you want to get
- *
- * Return: the value of the bit at index index or -1 if an error occured
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count;
-	unsigned long int bitMask, result;
+	int bit_val;
 
-	count = bitCount(n);
-	if (index > count)
+	if (index > 63)
 		return (-1);
-	bitMask = 1 << count;
-	result = n << (count - index);
-	if (result & bitMask)
-		return (1);
 
-	return (0);
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
+
