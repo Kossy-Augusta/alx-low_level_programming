0fd4a86869a18151b1c008a2d5fffcac481af316
Kossy-Augusta <kossyudogu@gmail.com>
2024-01-16 23:50:28 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,35 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * create_list - creates a node of a linked list
- * @key: The key
- * @value: value of the key
- *
- * Return: a pointer to the single node created or NULL is something goes wrong
- */
-hash_node_t *create_list(const char *key, const char *value)
-{
-	hash_node_t *node;
-
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (NULL);
-	node->key = (char *) malloc(strlen(key) + 1);
-	if (node->key == NULL)
-		return (NULL);
-	strcpy(node->key, key);
-
-	node->value = (char *) malloc(strlen(value) + 1);
-	if (node->value == NULL)
-		return (NULL);
-	strcpy(node->value, value);
-
-	node->next = NULL;
-
-	return (node);
-}
-
-/**
  * hash_table_set - a function that adds an element to the hash table
  * @ht: The hash table
  * @key: The string to be hashed
@@ -40,43 +11,48 @@ hash_node_t *create_list(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
-	hash_node_t *current_index;
-	hash_node_t *temp;
+	hash_node_t *current_index, *temp;
 	hash_node_t *node;
+	char *value_copy;
 	unsigned long int index;
 
 	/*Check if key is an empty string*/
-	if (key == NULL)
-		return (0);
-	node = create_list(key, value);
-	if (node == NULL)
+	if (key == NULL || ht == NULL || *key == '\0'|| value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current_index = ht->array[index];
-	if (current_index == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+	temp = current_index;
+	while (temp != NULL)
 	{
-		/* Check if index is out of array bound*/
-		if (index > ht->size)
-			return (0);
-		current_index = node;
-		return (1);
-	}
-	else
-	{
-		temp = ht->array[index];
-		while (temp != NULL)
+		if (strcmp(temp->key, key) == 0)
 		{
-			/*update key value if key already exists*/
-			if (strcmp(temp->key, key) == 0)
-			{
-				temp->value = (char *)value;
-				return (1);
-			}
-			temp = temp->next;
+			free(temp->value);
+			temp->value = value_copy;
+			return(1);
 		}
-		node->next = current_index;
-		current_index = node;
+		temp = temp->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	node->value = value_copy;
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(value_copy);
+		free(node);
+		return (0);
+	}
+	node->next = current_index;
+	current_index = node;
+
 		return (1);
 
-	}
 }
